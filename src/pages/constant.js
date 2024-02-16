e51022b16548bbd7dd1ae96428dc1c98c61b610d
Jonah Aden <jka2154@columbia.edu>
2024-02-16 16:03:29 -0500

    started outling constant display page

--- src/pages/constant.js
+++ src/pages/constant.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import './../pages-style/constant.css';
+import LMCCMockup from '../assets/LMCC Mockup.png';
 
-function constant() {
+function Constant() {
   return (
     <div>
       <div className="container">
-        <div className="column">Left</div>
-        <div className="column">Middle</div>
-        <div className="column">Right</div>
+        <div className="column Astronaut">
+          <h3>Astronaut</h3>
+          <p>This section will display the following:</p>
+          <ol>
+            <li>Astronaut Biometrics</li>
+            <li>State of Oxygen and Suit data</li>
+            <li>A video feed of the Astronauts POV</li>
+          </ol>
+        </div>
+        <div className="column Mission">
+          <h3>Mission</h3>
+          <p>This section will display the following:</p>
+          <ol>
+            <li>The Map with checkpoints</li>
+            <li>Elapsed Mission Time</li>
+            <li>Mission Objective Checklist</li>
+          </ol>
+          <img src={LMCCMockup} alt="LMCCMockup" />
+        </div>
+        <div className="column Rover">
+          <h3>Rover</h3>
+          <p>This section will display the following:</p>
+          <ol>
+            <li>Rover Camera Feed</li>
+            <li>State of Rover battery, connection, tire pressure</li>
+            <li>Rover Thermal Camera Feed</li>
+          </ol>
+        </div>
       </div>
     </div>
   );
 }
 
-export default constant;
+export default Constant;
