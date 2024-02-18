e527c3c8b1fe76a222ef7ef8a624ed18adeb1eb7
Jonah Aden <jka2154@columbia.edu>
2024-02-18 17:02:02 -0500

    Added video feed (gif)

--- src/pages/constant.js
+++ src/pages/constant.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import './../pages-style/constant.css';
 import LMCCMockup from '../assets/LMCC Mockup.png';
+import AstronautGif from '../assets/astronautstream.gif';
 
 function Constant() {
   return (
     <div>
       <div className="container">
         <div className="column Astronaut">
-          <h3>Astronaut</h3>
-          <p>This section will display the following:</p>
-          <ol>
-            <li>Astronaut Biometrics</li>
-            <li>State of Oxygen and Suit data</li>
-            <li>A video feed of the Astronauts POV</li>
-          </ol>
+          <div className="header-banner">
+            <h2>Astronaut Data</h2>
+          </div>
+          <div className="gif-container">
+            <img src={AstronautGif} alt="Astronaut POV GIF" />
+          </div>
+          <div className="subContainer">
+            <div className="column SuitData">
+              <div className="smallHB">
+                <h3>Suit Data</h3>
+              </div>
+            </div>
+            <div className="column Biometrics">
+              <div className="smallHB">
+                <h3>Biometrics</h3>
+              </div>
+              <p>This section will display the following:</p>
+              <ol>
+                <li>Astronaut Biometrics</li>
+                <li>State of Oxygen and Suit data</li>
+                <li>A GIF of the Astronaut's POV</li>
+              </ol>
+            </div>
+          </div>
         </div>
         <div className="column Mission">
           <h3>Mission</h3>
@@ -23,7 +41,7 @@ function Constant() {
             <li>Elapsed Mission Time</li>
             <li>Mission Objective Checklist</li>
           </ol>
-          <img src={LMCCMockup} alt="LMCCMockup" />
+          <img className="lmccImg" src={LMCCMockup} alt="LMCCMockup" />
         </div>
         <div className="column Rover">
           <h3>Rover</h3>
