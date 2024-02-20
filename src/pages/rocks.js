70b1b0c8ec0dd0ba5f8db51cd618c6e26c495bef
Jonah Aden <jka2154@columbia.edu>
2024-02-19 20:40:34 -0500

    Added Finn's Warning System
    On rocks page, warning button triggers a warning popup. Warning mechanism will be triggered autonomously in production.

--- src/pages/rocks.js
+++ src/pages/rocks.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './../pages-style/rocks.css';
 
-function rocks() {
+function Rocks() {
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleShowWarning = () => {
+    setShowWarning(true);
+  };
+
+  const handleCloseWarning = () => {
+    setShowWarning(false);
+  };
+
   return (
     <div>
       <h1>Rocks</h1>
       <p>This page will display samples that have been collected, and what samples should be kept</p>
+      <button onClick={handleShowWarning}>Show Warning</button>
+
+      {showWarning && (
+        <div className="warning-modal">
+          <div className="warning-modal-content">
+            <span className="close" onClick={handleCloseWarning}>&times;</span>
+            <p>Warning: Are you sure you want to proceed?</p>
+            <button onClick={handleCloseWarning}>Yes, Proceed</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
-export default rocks;
+export default Rocks;
