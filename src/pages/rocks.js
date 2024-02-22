0b825a0e56ffffd48cb966a08d84e6eeb254d4f7
MattKGatune <151987123+MattKGatune@users.noreply.github.com>
2024-02-21 20:24:34 -0500

    Added Procedures

--- src/pages/rocks.js
+++ src/pages/rocks.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './../pages-style/rocks.css';
-
+import { ProcedureList } from '../helpers/ProcedureList';
+import ProcedureItem from '../components/ProcedureItem';
 function rocks() {
   return (
     <div>
       <h1>Rocks</h1>
       <p>This page will display samples that have been collected, and what samples should be kept</p>
+      <div className = "ProcedureList"> 
+      {ProcedureList.map((Item, key) =>{
+        return (
+          <ProcedureItem
+          key = {key}
+          name = {Item.name}
+          description = {Item.description}
+          />
+        )
+      })}
+      </div>
     </div>
   );
 }
