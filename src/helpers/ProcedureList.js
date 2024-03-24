5da1b30cbdfd68fb12530f05ed237f108bc3bc2c
MattKGatune <151987123+MattKGatune@users.noreply.github.com>
2024-03-24 16:12:46 -0400

    ingress images added

--- src/helpers/ProcedureList.js
+++ src/helpers/ProcedureList.js
@@ -1,20 +1,22 @@
+import image1 from '../assets/dog1.png';
+import image2 from '../assets/dog2.png';
+import image3 from '../assets/dog3.png';
+
 export const ProcedureList = [
-
     {
-        name: "Procedure 1:",
-        description: "Blah",
-
+      name: 'Procedure 1:',
+      description: 'Blah',
+      image: image1,
     },
-
     {
-        name: "Procedure 2:",
-        description: "Blah Blah",
-
+      name: 'Procedure 2:',
+      description: 'Blah Blah',
+      image: image2,
     },
-
     {
-        name: "Procedure 3:",
-        description: "Blah Blah Blah",
-
+      name: 'Procedure 3:',
+      description: 'Blah Blah Blah',
+      image: image3,
     },
-];
+    // Add more procedures as needed
+  ];
