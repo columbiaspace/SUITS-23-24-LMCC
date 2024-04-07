85d8d233122588fd1e48f5141cda11335fec9bf8
Shirley <elenagrenewherd7856@gmail.com>
2024-04-07 15:19:57 -0400

    Update rocks.js

--- src/pages/rocks.js
+++ src/pages/rocks.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import './../pages-style/rocks.css';
+import MapGif from '../assets/MapGif.gif';
+import SampleMap from "../assets/SampleMap.png";
 import { populateRockData } from './../helpers/RockData.js';
 
 function Rocks() {
@@ -17,20 +19,43 @@ function Rocks() {
             <div className="maps-container">
               <div className="subcontainer astronaut-loc-container">
                 <h2>Astronaut Location</h2>
+                <div className="image astro-loc-gif">
+                  <img src={MapGif} alt="Map GIF"/>
+                </div>
               </div>
-              <div className="subcontainer sampling-map-container">
+              <div className="subcontainer sample-map-container">
                 <h2>Sample Map</h2>
+                <div className="image sample-map-image">
+                  <img src={SampleMap} alt="Sample Map"/>
+                </div>
               </div>
             </div>
             <div className="points-of-interest-container">
               <h2>Points of Interest</h2>
+              <table>
+                <tr> {/* table row */}
+                  <td>A</td>
+                  <td><button>Set as Dest</button></td>
+                </tr>
+                <tr>
+                  <td>B</td>
+                  <td><button>Set as Dest</button></td>
+                </tr>
+                <tr>
+                  <td>C</td>
+                  <td><button>Set as Dest</button></td>
+                </tr>
+                <tr>
+                  <td>D</td>
+                  <td><button>Set as Dest</button></td>
+                </tr>
+              </table>
             </div>
           </div>
           { /* Rocks Column on the right half */ }
           <div className="rocks-column">
             <h2>Table of Rocks</h2>
             <div id="rockContainer"></div>
-          
           </div>
         </div>
         <footer className="bottom-nav-container">
